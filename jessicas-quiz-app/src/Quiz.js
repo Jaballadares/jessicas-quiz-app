1436ad7d75305d983e4e9f831ea1b0e67b578725
Shaun Yee <yeezy@Shauns-MacBook-Pro.local>
2018-01-08 22:14:54 -0800

    adds basic next button once a question is answered

--- jessicas-quiz-app/src/Quiz.js
+++ jessicas-quiz-app/src/Quiz.js
@@ -10,13 +10,25 @@ class Quiz extends Component {
       quiz: quiz,
       currentPosition: 0
     };
+    this.nextQuestion = this.nextQuestion.bind(this)
   }
+
+  nextQuestion() {
+    if(this.state.currentPosition < this.state.quiz.length -1){
+    this.setState({currentPosition: this.state.currentPosition +1})
+  } else {
+    alert("you are finished with the quiz!! YaY!!")
+  }
+};
+
   render(){
     console.log(quiz)
     return(
       <div className="quiz">
         <h2>Question: {this.state.quiz[this.state.currentPosition].question}</h2>
         <Answers options={this.state.quiz[this.state.currentPosition].options} answer={this.state.quiz[this.state.currentPosition].answer}/>
+
+        <button onClick={this.nextQuestion}>Click to Answer</button>
 
       </div>
 
